d8f851168a68f1c3d24b99c4f453aa0a81d7f3b7
MarcelloLins <marcello.grechi@gmail.com>
2014-10-26 23:52:11 -0200

    Finished base of the categories pipeline
    Structure of the second phase of the scrapper is done

--- AppStoreCrawler/AppStoreCategoriesWorker/CategoriesWorker.cs
+++ AppStoreCrawler/AppStoreCategoriesWorker/CategoriesWorker.cs
@@ -1,9 +1,13 @@
-﻿using SharedLibrary.ConfigurationReader;
+﻿using SharedLibrary;
+using SharedLibrary.AWS;
+using SharedLibrary.ConfigurationReader;
 using SharedLibrary.Logging;
+using SharedLibrary.Parsing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AppStoreCategoriesWorker
@@ -17,16 +21,88 @@ namespace AppStoreCategoriesWorker
         private static string _categoriesQueueName;
         private static string _awsKey;
         private static string _awsKeySecret;
+        private static int    _maxRetries;
+        private static int    _maxMessagesPerDequeue;
+
+        // Control Variables
+        private static int    _hiccupTime = 1000;
 
         static void Main(string[] args)
         {
+            // Creating Needed Instances
+            RequestsHandler httpClient = new RequestsHandler ();
+            AppStoreParser  parser     = new AppStoreParser ();
+            _logger                    = new LogWrapper ();
+
+            // Loading Configuration
+            _logger.LogMessage ("Loading Configurations from App.config");
+            LoadConfiguration ();
+
+            // AWS Queue Handler
+            _logger.LogMessage ("Initializing Queues");
+            AWSSQSHelper categoriesUrlQueue = new AWSSQSHelper (_categoriesQueueName, 10, _awsKey, _awsKeySecret);
+
+            // Setting Error Flag to No Error ( 0 )
+            System.Environment.ExitCode = 0;
+
+            // Initialiazing Control Variables
+            int fallbackWaitTime = 1;
+
+            _logger.LogMessage ("Started Processing Category Urls");
+
+            do
+            {
+                try
+                {
+                    // Dequeueing messages from the Queue
+                    if (!categoriesUrlQueue.DeQueueMessages())
+                    {
+                        Thread.Sleep (_hiccupTime); // Hiccup                   
+                        continue;
+                    }
+
+                    // Checking for no message received, and false positives situations
+                    if (!categoriesUrlQueue.AnyMessageReceived())
+                    {
+                        // If no message was found, increases the wait time
+                        int waitTime;
+                        if (fallbackWaitTime <= 12)
+                        {
+                            // Exponential increase on the wait time, truncated after 12 retries
+                            waitTime = Convert.ToInt32 (Math.Pow (2, fallbackWaitTime) * 1000);
+                        }
+                        else // Reseting Wait after 12 fallbacks
+                        {
+                            waitTime         = 2000;
+                            fallbackWaitTime = 0;
+                        }
+
+                        fallbackWaitTime++;
+
+                        // Sleeping before next try
+                        Console.WriteLine ("Fallback (seconds) => " + waitTime);
+                        Thread.Sleep (waitTime);
+                        continue;
+                    }
+
+                    // Reseting fallback time
+                    fallbackWaitTime = 1;
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogMessage (ex);
+                }
+
+            } while (true);
         }
 
         private static void LoadConfiguration ()
         {
-            _categoriesQueueName = ConfigurationReader.LoadConfigurationSetting<String> ("AWSCategoriesQueue", String.Empty);
-            _awsKey              = ConfigurationReader.LoadConfigurationSetting<String> ("AWSKey"            , String.Empty);
-            _awsKeySecret        = ConfigurationReader.LoadConfigurationSetting<String> ("AWSKeySecret"      , String.Empty);
+            _maxRetries            = ConfigurationReader.LoadConfigurationSetting<int>    ("MaxRetries"           , 0);
+            _maxMessagesPerDequeue = ConfigurationReader.LoadConfigurationSetting<int>    ("MaxMessagesPerDequeue", 10);
+            _categoriesQueueName   = ConfigurationReader.LoadConfigurationSetting<String> ("AWSCategoriesQueue"   , String.Empty);
+            _awsKey                = ConfigurationReader.LoadConfigurationSetting<String> ("AWSKey"               , String.Empty);
+            _awsKeySecret          = ConfigurationReader.LoadConfigurationSetting<String> ("AWSKeySecret"         , String.Empty);
         }
     }
 }

a90daa1167947a6c2f945e27be2e5c08464c577e
MarcelloLins <marcello.grechi@gmail.com>
2014-10-27 18:38:54 -0200

    Checkpoint !
    + Scrapping of individual character urls
    + Retry logic and logging
    + Proper exception handling

--- AppStoreCrawler/AppStoreUrlsWorker/AppUrlsWorker.cs
+++ AppStoreCrawler/AppStoreUrlsWorker/AppUrlsWorker.cs
@@ -7,7 +7,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace AppStoreUrlsWorker
 {
@@ -53,8 +55,97 @@ namespace AppStoreUrlsWorker
 
             do
             {
+                try
+                {
+                    // Dequeueing messages from the Queue
+                    if (!charactersUrlQueue.DeQueueMessages())
+                    {
+                        Thread.Sleep (_hiccupTime); // Hiccup                   
+                        continue;
+                    }
 
+                    // Checking for no message received, and false positives situations
+                    if (!charactersUrlQueue.AnyMessageReceived())
+                    {
+                        // If no message was found, increases the wait time
+                        int waitTime;
+                        if (fallbackWaitTime <= 12)
+                        {
+                            // Exponential increase on the wait time, truncated after 12 retries
+                            waitTime = Convert.ToInt32 (Math.Pow (2, fallbackWaitTime) * 1000);
+                        }
+                        else // Reseting Wait after 12 fallbacks
+                        {
+                            waitTime         = 2000;
+                            fallbackWaitTime = 0;
+                        }
 
+                        fallbackWaitTime++;
+
+                        // Sleeping before next try
+                        Console.WriteLine ("Fallback (seconds) => " + waitTime);
+                        Thread.Sleep (waitTime);
+                        continue;
+                    }
+
+                    // Reseting fallback time
+                    fallbackWaitTime = 1;
+
+                    // Iterating over dequeued Messages
+                    foreach (var characterUrl in charactersUrlQueue.GetDequeuedMessages ())
+                    {
+                        // Console Feedback
+                        _logger.LogMessage ("Started Parsing Url : " + characterUrl.Body);
+
+                        try
+                        {
+                            // Retries Counter
+                            int retries = 0;
+                            string htmlResponse;
+
+                            // Retrying if necessary
+                            do
+                            {
+                                // Executing Http Request for the Category Url
+                                htmlResponse = httpClient.Get (characterUrl.Body);
+
+                                if (String.IsNullOrEmpty (htmlResponse))
+                                {
+                                    _logger.LogMessage ("Retrying Request for Character Page", "Request Error", BDC.BDCCommons.TLogEventLevel.Error);
+                                    retries++;
+                                }
+
+                            } while (String.IsNullOrWhiteSpace (htmlResponse) && retries <= _maxRetries);
+
+                            // Checking if retries failed
+                            if (String.IsNullOrWhiteSpace (htmlResponse))
+                            {
+                                // Deletes Message and moves on
+                                charactersUrlQueue.DeleteMessage (characterUrl);
+                                continue;
+                            }
+
+                            // If the request worked, parses the Urls out of the page
+                            foreach (string numericUrls in parser.ParseNumericUrls (htmlResponse))
+                            {
+                                // Enqueueing Urls
+                                charactersUrlQueue.EnqueueMessage (HttpUtility.HtmlDecode (numericUrls));
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            _logger.LogMessage (ex);
+                        }
+                        finally
+                        {
+                            charactersUrlQueue.DeleteMessage (characterUrl);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogMessage (ex);
+                }
 
             } while (true);
 
